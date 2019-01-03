4f021a45b2b11fdae98ad223b58b6a2cc4dd7c79
Aleksandra Hannum <aleksandrahannum@gmail.com>
2019-01-03 10:11:21 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const cookieButton = document.querySelector('.cookie-buttonn')
+
+const cookieBar = document.querySelector('.cookie-bar') 
+
+cookieButton.addEventListener('click', () => {
+    cookieBar.style.display = "none";});
+
