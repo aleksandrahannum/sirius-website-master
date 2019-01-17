f9c8b383bcee4459b0ab5860201439f55ea4cad1
unknown <jakubmx@gmail.com>
2019-01-17 15:45:02 +0100

    hide/show cookie-bar accordingly to user action

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
 const cookieButton = document.querySelector('.cookie-buttonn')
-
 const cookieBar = document.querySelector('.cookie-bar') 
 
-cookieButton.addEventListener('click', () => {
-    cookieBar.style.display = "none";});
+function hideCookieBar() {
+    cookieBar.style.display = "none";
+    cookieBar.setAttribute('aria-hidden', true);
+}
 
+function showCookieBar() {
+    cookieBar.style.display = "flex";
+    cookieBar.setAttribute('aria-hidden', false);
+}
+
+if (!localStorage.getItem('cookie')) {
+    showCookieBar();
+}
+
+cookieButton.addEventListener('click', () => {
+    hideCookieBar();
+    localStorage.setItem('cookie', 'allowed');
+});
